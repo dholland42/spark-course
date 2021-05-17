d77a9763e2f87de2b82e22fd80b6d985925ddfb4
Daniel Holland <danielholland414@gmail.com>
2021-05-16 20:05:30 -0400

    initial stuff

---
+++ Section 1/histogram.py
@@ -0,0 +1,29 @@
+"""A simple script to count rating values from the movielens 100k dataset."""
+
+import collections
+
+import click
+from pyspark import SparkConf, SparkContext
+
+def local_context(appname):
+   """Create a local spark context."""
+   conf = SparkConf().setMaster("local").setAppName(appname)
+   sc = SparkContext(conf=conf)
+   return sc
+
+
+@click.command()
+@click.option("--filename", "-f", required=True)
+def main(filename):
+   sc = local_context(appname="RatingsHistogram")
+   lines = sc.textFile(filename)
+   ratings = lines.map(lambda x: x.split()[2])
+   result = ratings.countByValue()
+
+   sortedResults = collections.OrderedDict(sorted(result.items()))
+   for key, value in sortedResults.items():
+      print(f"{key} {value}")
+
+
+if __name__ == "__main__":
+   main()
